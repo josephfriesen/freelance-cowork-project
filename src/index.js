98088d25c17d9e139aedd42f83445ff2dd3303de
Joseph Friesen <friesen.josephc@gmail.com>
2018-09-21 15:52:03 -0700

    Add media queries to syles.scss to rearrange and resize page elements, hide nav buttons on smaller viewports.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import './sass/styles.scss';
 import 'bootstrap';
+import $ from 'jquery';
+
+$(document).ready(function(){
+  $("#toggle-nav").click(function(){
+    $(".nav-container").slideToggle();
+  });
+});
